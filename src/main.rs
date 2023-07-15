daac1cceb149a156aa878bb237258e6c8cee233e
zudo <44712315+zudo@users.noreply.github.com>
2023-07-16 00:06:21 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
-const THRESHOLD: usize = 5;
-const MULTIPLIER: f32 = 1.5; // VIRTUAL_CPS = CPS * MULTIPLIER
-const DELAY: u64 = 1000; // ms
+const THRESHOLD: f32 = 5.0;
+const DELAY: u64 = 100;
 fn main() {
     let counter = Arc::new(Mutex::new(0.0));
-    let cps = Arc::new(Mutex::new(0.0));
+    let mut cps = 0.0;
     {
         let counter = counter.clone();
         thread::spawn(move || {
@@ -28,28 +27,15 @@ fn main() {
             }
         });
     }
-    {
-        let counter = counter.clone();
-        let cps = cps.clone();
-        thread::spawn(move || loop {
-            thread::sleep(Duration::from_millis(DELAY));
-            let mut cps = cps.lock().unwrap();
-            let mut counter = counter.lock().unwrap();
-            *cps += *counter;
-            *counter = 0.0;
-            if *cps > THRESHOLD as f32 {
-                println!("click");
-            }
-        });
-    }
     loop {
-        thread::sleep(Duration::from_secs(1));
-        let mut cps = cps.lock().unwrap();
-        let counter = counter.lock().unwrap();
-        let cps_float = *cps as f32 / MULTIPLIER;
-        let counter_float = *counter as f32;
-        println!("CPS: {:.2}", cps_float);
-        println!("Counter: {:.2}", counter_float);
-        *cps /= 2.0;
+        thread::sleep(Duration::from_millis(DELAY));
+        let mut counter = counter.lock().unwrap();
+        cps += *counter * (1000 / DELAY) as f32;
+        cps /= 2.0;
+        *counter = 0.0;
+        println!("CPS: {:.2}", cps);
+        if cps > THRESHOLD {
+            println!("click");
+        }
     }
 }
