fd2780d1152f5317643b33c394b4cb40f5a2f0ee
zudo <44712315+zudo@users.noreply.github.com>
2023-07-15 22:24:51 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rdev::listen;
+use rdev::Event;
 fn main() {
     println!("Hello, world!");
+    // This will block.
+    if let Err(error) = listen(callback) {
+        println!("Error: {:?}", error)
+    }
+    fn callback(event: Event) {
+        println!("My callback {:?}", event);
+        match event.name {
+            Some(string) => println!("User wrote {:?}", string),
+            None => (),
+        }
+    }
 }
