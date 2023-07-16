a40545b2603fa47682b722f42248458b7b48ba82
zudo <44712315+zudo@users.noreply.github.com>
2023-07-16 06:59:45 +0200

    state

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,32 @@ use rdev::Event;
 use rdev::EventType::ButtonPress;
 use rdev::EventType::ButtonRelease;
 use std::collections::VecDeque;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
-const THRESHOLD: f32 = 5.0;
+const THRESHOLD: f32 = 6.0;
 const MULTIPLIER: f32 = 2.0;
 const DELAY: Duration = Duration::from_millis(50);
 fn main() {
     let clicks = Arc::new(Mutex::new(VecDeque::new()));
     let clicks_simulated = Arc::new(Mutex::new(VecDeque::<Instant>::new()));
+    let state = Arc::new(AtomicBool::new(false));
     {
         let clicks = clicks.clone();
+        let state = state.clone();
         thread::spawn(move || {
             let callback = move |event: Event| match event.event_type {
                 ButtonPress(Left) => {
                     clicks.lock().unwrap().push_back(Instant::now());
+                    state.store(true, Ordering::Relaxed);
                 }
-                ButtonRelease(Left) => {}
+                ButtonRelease(Left) => {
+                    state.store(false, Ordering::Relaxed);
+                }
                 _ => {}
             };
             if let Err(e) = listen(callback) {
@@ -64,17 +71,19 @@ fn main() {
         if let Some(latest) = mg_clicks.back() {
             if cps > THRESHOLD && latest.elapsed() < DELAY {
                 drop(mg_clicks);
-                click();
+                click(state.clone());
                 clicks_simulated.lock().unwrap().push_back(Instant::now());
                 clicks.lock().unwrap().pop_back();
             }
         }
     }
 }
-fn click() {
-    thread::spawn(|| {
-        simulate(&ButtonRelease(Left)).unwrap();
-        thread::sleep(Duration::from_millis(1));
+fn click(state: Arc<AtomicBool>) {
+    thread::spawn(move || {
+        if state.load(Ordering::Relaxed) {
+            simulate(&ButtonRelease(Left)).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
         simulate(&ButtonPress(Left)).unwrap();
         thread::sleep(Duration::from_millis(1));
         simulate(&ButtonRelease(Left)).unwrap();
