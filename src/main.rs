43e8a7d0e2ebe341c1b36493951ca50fa50390e2
zudo <44712315+zudo@users.noreply.github.com>
2023-07-16 06:17:51 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,11 @@ use std::thread;
 use std::time::Duration;
 use std::time::Instant;
 const THRESHOLD: f32 = 5.0;
-const MULTIPLIER: f32 = 4.0;
+const MULTIPLIER: f32 = 3.0;
 const DELAY: Duration = Duration::from_millis(100);
 fn main() {
     let clicks = Arc::new(Mutex::new(VecDeque::new()));
+    let clicks_simulated = Arc::new(Mutex::new(VecDeque::<Instant>::new()));
     {
         let clicks = clicks.clone();
         thread::spawn(move || {
@@ -32,17 +33,26 @@ fn main() {
     }
     {
         let clicks = clicks.clone();
+        let clicks_simulated = clicks_simulated.clone();
         thread::spawn(move || loop {
             thread::sleep(Duration::from_millis(1));
-            let mut clicks = clicks.lock().unwrap();
-            while clicks.len() > 0 && clicks[0].elapsed() > Duration::from_secs(1) {
-                clicks.pop_front();
+            let mut mg_clicks = clicks.lock().unwrap();
+            let mut mg_clicks_simulated = clicks_simulated.lock().unwrap();
+            while mg_clicks.len() > 0 && mg_clicks[0].elapsed() > Duration::from_secs(1) {
+                mg_clicks.pop_front();
             }
-            let cps = clicks.len();
+            while mg_clicks_simulated.len() > 0
+                && mg_clicks_simulated[0].elapsed() > Duration::from_secs(1)
+            {
+                mg_clicks_simulated.pop_front();
+            }
+            let cps = mg_clicks.len();
+            let cps_simulated = mg_clicks_simulated.len();
             println!(
-                "CPS: {}, {:.0?}",
+                "CPS: {}, SIM: {}, {:.0?}",
                 cps,
-                clicks.iter().map(|x| x.elapsed()).collect::<Vec<_>>()
+                cps_simulated,
+                mg_clicks.iter().map(|x| x.elapsed()).collect::<Vec<_>>()
             );
         });
     }
@@ -51,24 +61,22 @@ fn main() {
         let millis = (1000.0 / (cps * MULTIPLIER)) as u64;
         thread::sleep(Duration::from_millis(millis));
         let mg_clicks = clicks.lock().unwrap();
-        println!(
-            "CPS: {}, {:.0?}",
-            cps,
-            mg_clicks.iter().map(|x| x.elapsed()).collect::<Vec<_>>()
-        );
         if let Some(latest) = mg_clicks.back() {
             if cps > THRESHOLD && latest.elapsed() < DELAY {
                 drop(mg_clicks);
                 click();
+                clicks_simulated.lock().unwrap().push_back(Instant::now());
                 clicks.lock().unwrap().pop_back();
             }
         }
     }
 }
 fn click() {
-    simulate(&ButtonRelease(Left)).unwrap();
-    thread::sleep(Duration::from_millis(1));
-    simulate(&ButtonPress(Left)).unwrap();
-    thread::sleep(Duration::from_millis(1));
-    simulate(&ButtonRelease(Left)).unwrap();
+    thread::spawn(|| {
+        simulate(&ButtonRelease(Left)).unwrap();
+        thread::sleep(Duration::from_millis(1));
+        simulate(&ButtonPress(Left)).unwrap();
+        thread::sleep(Duration::from_millis(1));
+        simulate(&ButtonRelease(Left)).unwrap();
+    });
 }
